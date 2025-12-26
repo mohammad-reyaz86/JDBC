237d827f4950543957df9ec38b2872b3fd9d7598
Mohammad Reyaz <165875108+mohammad-reyaz86@users.noreply.github.com>
2025-12-26 19:14:36 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,37 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.sql.*;
+public class Main {
+    public static void main(String[] args) throws ClassNotFoundException {
+        String url = "jdbc:mysql://localhost:3306/mydatabase";
+        String username = "root";
+        String password = "#Reyazlove1";
+        String query = "INSERT INTO employee(id, name, job_title, salary) VALUES(3, 'Harshit', 'Full Stack Developer',87000.0);";
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            System.out.println("Driver loaded successfully");
+        }catch (ClassNotFoundException e){
+            System.out.println(e.getMessage());
+        }
+
+        try{
+            Connection con = DriverManager.getConnection(url, username , password);
+            System.out.println("connection Established successfully");
+            Statement stmt = con.createStatement();
+            int rowsaffected =  stmt.executeUpdate(query);
+
+            if(rowsaffected>0){
+                System.out.println("Insert Successfully. "+rowsaffected+" row(s) affected.");
+            }else{
+                System.out.println("Insertion failed");
+            }
+
+            stmt.close();
+            con.close();
+            System.out.println();
+            System.out.println("connection closed successfully");
+        }catch (SQLException e){
+            System.out.println(e.getMessage());
+        }
+    }
+}
